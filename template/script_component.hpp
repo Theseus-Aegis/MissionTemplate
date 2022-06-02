5dfbbd5682cddbc822933d0701f15611fa7daff3
jonpas <jonpas33@gmail.com>
2022-06-02 15:37:20 +0200

    Prepare release 3.6.2

--- template/script_component.hpp
+++ template/script_component.hpp
@@ -4,7 +4,7 @@
 // Version
 #define MAJOR 3
 #define MINOR 6
-#define PATCHLVL 1
+#define PATCHLVL 2
 
 // Mission
 #define MISSION_TYPES ["Contract", "Non-Contract", "Training", "Special", "PvP", "Gimmick"]
