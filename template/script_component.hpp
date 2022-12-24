906241b599e1fc30b52c4abaf74e15cccc4fa826
jonpas <jonpas33@gmail.com>
2022-12-24 20:07:44 +0100

    Fix mission defines

--- template/script_component.hpp
+++ template/script_component.hpp
@@ -1,6 +1,6 @@
-#define MISSION_NAME    "MISSION NAME"
-#define MISSION_SUMMARY "MISSION SUMMARY"
-#define MISSION_AUTHOR  "YOUR FULL NAME"
+#define MISSION_NAME    MISSION NAME
+#define MISSION_SUMMARY MISSION SUMMARY
+#define MISSION_AUTHOR  YOUR FULL NAME
 #define MISSION_TYPE    0 // 0: Contract, 1: Non-Contract, 2: Training, 3: Special, 4: PvP, 5: Gimmick
 
 //#define DEBUG_MODE_FULL
