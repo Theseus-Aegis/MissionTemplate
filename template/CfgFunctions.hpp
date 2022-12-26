3f9d8bb7b82a586007b583a9bced692e9f4b0a62
jonpas <jonpas33@gmail.com>
2022-12-26 03:11:30 +0100

    Add RECOMPILE and DFUNC macros

--- template/CfgFunctions.hpp
+++ template/CfgFunctions.hpp
@@ -1,11 +1,10 @@
 class CfgFunctions {
     class ADDON {
-        tag = QUOTE(ADDON);
-        class functions {
-            file = "functions";
-            //recompile = 1; // Debug - Requires allowFunctionsRecompile = 1; in description.ext
-            // Reference scripts/README.md for usage instructions
-            class briefing;
+        class Template {
+            DFUNC(briefing);
+        };
+        class Custom {
+            //DFUNC(myFunction);
         };
     };
 };
