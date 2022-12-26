757a82c270d825c6acb9c9652136fdc502f09c10
jonpas <jonpas33@gmail.com>
2022-12-26 02:09:16 +0100

    Set Contract type and settings back as default

--- template/script_component.hpp
+++ template/script_component.hpp
@@ -1,8 +1,8 @@
 #define MISSION_NAME     MISSION NAME
 #define MISSION_SUMMARY  MISSION SUMMARY
 #define MISSION_AUTHOR   YOUR FULL NAME
-#define MISSION_TYPE     3 // 0: Contract, 1: Non-Contract, 2: Training, 3: Special, 4: PvP, 5: Gimmick
-#define MISSION_SETTINGS 2 // 0: Contract, 1: Non-Contract, 2: SOG
+#define MISSION_TYPE     0 // 0: Contract, 1: Non-Contract, 2: Training, 3: Special, 4: PvP, 5: Gimmick
+#define MISSION_SETTINGS 0 // 0: Contract, 1: Non-Contract, 2: SOG
 
 //#define DEBUG_MODE_FULL
 
